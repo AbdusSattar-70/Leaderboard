6a97d3be329d2d58cb25f580c36826bdd8ac0bbc
Abdus Sattar <sasorganicagro@gmail.com>
2023-03-14 15:24:35 +0600

    Change game ID

--- src/modules/url.js
+++ src/modules/url.js
@@ -1,2 +1,2 @@
-const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/90XLzZGNQ701ocoehEpZ/scores/';
+const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1XSh9O6EI1Unx10Kgez8/scores/';
 export default URL;
